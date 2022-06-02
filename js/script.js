5cc3fef0c672496a83eba7374fd6622d8723778b
adji <adjidibor@gmail.com>
2022-06-02 16:35:59 +0100

    mon project final

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ===============================typing animation ================================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Administrateur Système"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
